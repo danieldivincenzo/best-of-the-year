9e88f0c59f0e19afb0ab44fd9c3a89ea40d1197e
danieldivincenzo <danieldivincenzo14@gmail.com>
2025-09-10 16:45:39 +0200

    modificati template

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -7,10 +7,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.controller.model.Movie;
 import com.example.demo.controller.model.Song;
+
 
 
 @Controller
@@ -18,19 +18,37 @@ import com.example.demo.controller.model.Song;
 public class MainController {
 
     @GetMapping("/")
-    public String home(@RequestParam(required=false) String nome, Model model) {
+    public String home(Model model) {
 
-        model.addAttribute("nome", nome);
+        model.addAttribute("nome", "Daniel");
         return "home";
     }
     
+    Movie m1 = new Movie(1, "The Matrix");
+    Movie m2 = new Movie(2, "Inception");
+    Movie m3 = new Movie(3, "Interstellar");
+    Movie m4 = new Movie(4, "The Truman Show");
+
     private List<Movie> getBestMovies(){
         List<Movie> movies = new ArrayList<>();
+        movies.add(m1);
+        movies.add(m2);
+        movies.add(m3);
+        movies.add(m4);
         return movies;
     }
 
+    Song s1 = new Song(1, "Blinding Lights");
+    Song s2 = new Song(2, "The Hills");
+    Song s3 = new Song(3, "Congratulations");
+    Song s4 = new Song(4, "Rockstar");
+
     private List<Song> getBestSongs(){
         List<Song> songs = new ArrayList<>();
+        songs.add(s1);
+        songs.add(s2);
+        songs.add(s3);
+        songs.add(s4);
         return songs;
     }
 
